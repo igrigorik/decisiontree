2155771683d90318c164e74c7190b3b9da4e4c5b
Danielius <danielius@wisemonks.com>
2015-11-22 16:44:44 +0200

    Style examples

--- examples/discrete-id3.rb
+++ examples/discrete-id3.rb
@@ -1,15 +1,25 @@
 require 'rubygems'
 require 'decisiontree'
 
-# ---Discrete-----------------------------------------------------------------------------------------
+# ---Discrete---
 
 # Read in the training data
-training, attributes = [], nil
-File.open('data/discrete-training.txt','r').each_line { |line| 
+training = []
+File.open('data/discrete-training.txt', 'r').each_line do |line|
   data = line.strip.split(',')
   attributes ||= data
-  training.push(data.collect {|v| (v == 'will buy') || (v == "won't buy") ? (v == 'will buy' ? 1 : 0) : v})
-}
+  training_data = data.collect do |v|
+    case v
+    when 'will buy'
+      1
+    when "won't buy"
+      0
+    else
+      v
+    end
+  end
+  training.push(training_data)
+end
 
 # Remove the attribute row from the training data
 training.shift
@@ -18,17 +28,31 @@ training.shift
 dec_tree = DecisionTree::ID3Tree.new(attributes, training, 1, :discrete)
 dec_tree.train
 
-#---- Test the tree....
+# ---Test the tree---
 
 # Read in the test cases
-#    Note: omit the attribute line (first line), we know the labels from the training data
+# Note: omit the attribute line (first line), we know the labels from the training data
 test = []
-File.open('data/discrete-test.txt','r').each_line { |line| data = line.strip.split(',') 
-  test.push(data.collect {|v| (v == 'will buy') || (v == "won't buy") ? (v == 'will buy' ? 1 : 0) : v})
-}
+File.open('data/discrete-test.txt', 'r').each_line do |line|
+  data = line.strip.split(',')
+  test_data = data.collect do |v|
+    case v
+    when 'will buy'
+      1
+    when "won't buy"
+      0
+    else
+      v
+    end
+  end
+  training.push(test_data)
+end
 
 # Let the tree predict the output and compare it to the true specified value
-test.each { |t|   predict = dec_tree.predict(t); puts "Predict: #{predict} ... True: #{t.last}"; }
+test.each do |t|
+  predict = dec_tree.predict(t)
+  puts "Predict: #{predict} ... True: #{t.last}"
+end
 
 # Graph the tree, save to 'discrete.png'
-dec_tree.graph("discrete")
+dec_tree.graph('discrete')
